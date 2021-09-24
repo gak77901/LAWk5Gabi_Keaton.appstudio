be0f5fe9f68e37b6b506b8459a9b2f178621e592
gak77901 <gak77901@creighton.edu>
2021-09-24 09:50:20 -0500

    Commit Strings

---
+++ forms/Strings/Strings.js
@@ -0,0 +1,37 @@
+/*
+Create a variable named 'quoteString' and put this quote in it: 
+
+Failure is simply the opportunity to begin again this time more intelligently.
+
+Use the 'toUpperCase' string method to change the text in 'quoteString' to all upper case. 
+Output the changed string to the console using this format: 
+               Upper case string is: XXXXX
+               
+Create a variable named 'authorString' and put this text in it:
+               "- Henry Ford"
+
+Read about this string method 'concat()' Use it to create a new string variable 'completeString' that holds a string created by adding 'authorString'  to the end of 'quoteString'. 
+             Concatenated string is: XXXX
+
+Create a new variable 'found' that uses the string method 'includes()' to determine if "Henry" is in 'completeString' or not.
+              Henry was in completeString.
+              OR
+              Henry was NOT in completeString.
+              
+*/
+
+/*
+let quoteString = "Failure is simply the opportunity to begin again this time more intelligently." 
+console.log(`Upper case string is: ${quoteString.toUpperCase()}`) 
+
+let authorString = "- Henry Ford" 
+
+let completeString = quoteString.concat(authorString) 
+console.log(`Concatenated string is: ${completeString}`) 
+
+if (completeString.includes('Henry')) { 
+  console.log(`Henry was in completeString.`)
+  } else { 
+  console.log(`Henry was NOT in completeString.`)   
+  } 
+*/
